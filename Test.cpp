9d8f5d98d7ba6ff79083819167cedece75b09c00
ParticleCY <chengyun0407@126.com>
2018-06-04 17:42:23 +0800

    a

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,11 @@
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello World!";
+#include <stdio.h>
+int main(int argc, char *args[]){
+    int i, k;
+    printf("hello, world!\n");
+    printf("argc:%d\nargc:\n", argc);
+    for (i = 0; i < argc; i++){
+        printf("%d : %s\n", i, args[i]);
+    }
+    getchar();
     return 0;
 }
